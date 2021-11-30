581787abe819a6474b0d604a761aa4af7eede404
Andreas Boll Nielsen <andr687w@zbc.dk>
2021-11-30 11:43:36 +0100

    added components

---
+++ src/app/RecipeBook/RecipeList/Recipe-List/Recipe-List.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Recipe-List',
+  templateUrl: './Recipe-List.component.html',
+  styleUrls: ['./Recipe-List.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
