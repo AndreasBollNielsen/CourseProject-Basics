02da799db1d522badad83e25ded1180a54c37f96
AndreasBollNielsen <64074621+AndreasBollNielsen@users.noreply.github.com>
2021-11-30 15:38:55 +0100

    rearranged foldes and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './ShoppingList-Edit/ShoppingList-Edit.component';
-import { RecipeListComponent } from './Recipe-List/Recipe-List.component';
-import { RecipeDetailComponent } from './Recipe/Recipe-detail/Recipe-detail.component';
+import { ShoppingListComponent } from './ShoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './ShoppingList/ShoppingList-Edit/ShoppingList-Edit.component';
+import { RecipeListComponent } from './recipes/Recipe-List/Recipe-List.component';
+import { RecipeDetailComponent } from './recipes/Recipe-detail/Recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeItemComponent } from './Recipe-List/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './recipes/Recipe-List/recipe-item/recipe-item.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { RecipeItemComponent } from './Recipe-List/recipe-item/recipe-item.compo
       RecipeListComponent,
       RecipeDetailComponent,
       HeaderComponent,
-      RecipeItemComponent
+      RecipeItemComponent,
+      RecipesComponent
    ],
   imports: [
     BrowserModule
